6b02b3229f0551456062e6665aadf0d0ef7bc4ca
ismet <ismntr@icloud.com>
2024-09-21 02:39:26 +0300

    forms created

--- project4form/webapp/controller/View1form.controller.js
+++ project4form/webapp/controller/View1form.controller.js
@@ -1,12 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/core/UIComponent"
+], 
+function (Controller, History, UIComponent) {
     "use strict";
 
     return Controller.extend("project4form.controller.View1form", {
         onInit: function () {
+        },
 
+        onMaterialFormPress: function () {
+            var oRouter = UIComponent.getRouterFor(this);
+            oRouter.navTo("materialForm");
+        },
+
+        onServiceFormPress: function () {
+            var oRouter = UIComponent.getRouterFor(this);
+            oRouter.navTo("serviceForm");
+        },
+        onSubmit: function () {
+            var oModel = this.getView().getModel(); // OData Model
+            var oData = {
+                MaterialNo: this.getView().byId("materialNo").getValue(),
+                Supplier: this.getView().byId("supplier").getValue(),
+                // Diğer alanlar
+            };
+
+            oModel.create("/MaterialFormSet", oData, {
+                success: function () {
+                    sap.m.MessageToast.show("Form başarıyla gönderildi.");
+                },
+                error: function () {
+                    sap.m.MessageToast.show("Form gönderilirken hata oluştu.");
+                }
+            });
         }
     });
 });
